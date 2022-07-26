ba1d2e89625b54455de392f34ee5d8f241b2fd96
Adam Brock <a.paulbrock@gmail.com>
2022-07-26 15:47:33 -0400

    Small style changes.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,15 +8,14 @@ export default function Resume() {
       <header className="content-header">
         <h2>Resume</h2>
       </header>
-
       <section className="content-section">
         <div className="d-inline-block">
-          <div className="d-flex justify-content-around w-100 me-2">
+          <div className="d-flex justify-content-around w-100">
             <a
               rel="noreferrer"
               target="_blank"
               href="https://drive.google.com/file/d/1XNvKHmqF7imc3Rp4S_Loua_8FVEXx5Ds/view?usp=sharing"
-              className="d-flex flex-column align-items-center text-decoration-none resume-link"
+              className="d-flex flex-column align-items-center text-decoration-none resume-link mx-2"
             >
               <FontAwesomeIcon className="fa-7x" icon={faDisplay} />
               <h4 className="mt-2">View</h4>
@@ -25,7 +24,8 @@ export default function Resume() {
               rel="noreferrer"
               target="_blank"
               href="https://drive.google.com/uc?export=download&id=1XNvKHmqF7imc3Rp4S_Loua_8FVEXx5Ds"
-              className="d-flex flex-column align-items-center text-decoration-none resume-link"
+              className="d-flex flex-column align-items-center text-decoration-none resume-link mx-2"
+              download
             >
               <FontAwesomeIcon className="fa-7x" icon={faDownload} />
               <h4 className="mt-2">Download</h4>
